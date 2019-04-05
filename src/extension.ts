ce30f1b3893aeaf206448a7d2894ea4e38c4c32e
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-05 13:16:09 +0300

    Linter fixes.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from "vscode"; // tslint:disable-line:no-implicit-dependencies
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  const disposable = vscode.commands.registerCommand(
     "extension.helloWorld",
     () => {
       // The code you place here will be executed every time your command is executed
