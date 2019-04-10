20202a1faf5116c86caee10ce2d732b4266d5f76
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-04-10 16:08:47 +0300

    Clean-up.

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode"; // tslint:disable-line:no-implicit-dependencies
+
+const documentLanguageHoverProvider: vscode.HoverProvider = {
+  provideHover(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ) {
+    const text = `This is **${document.languageId}**.`;
+    return {
+      contents: [text],
+    };
+  },
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "helloworld" is now active!');
+  console.log("Extension active!");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("kimmo.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
+  const helloCmdDisposable = vscode.commands.registerCommand(
+    "kimmo.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello VS Code!");
+    }
+  );
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello VS Code!");
-  });
-
-  const disposable2 = vscode.commands.registerCommand(
+  const pantsOnFireCmdDisposable = vscode.commands.registerCommand(
     "kimmo.pantsOnFire",
     () => {
       vscode.window.showWarningMessage("Your pants are on fire!");
     }
   );
 
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(disposable2);
+  const docLanguageHoverDisposable = vscode.languages.registerHoverProvider(
+    ["javascript", "typescript"],
+    documentLanguageHoverProvider
+  );
+
+  context.subscriptions.push(
+    helloCmdDisposable,
+    pantsOnFireCmdDisposable,
+    docLanguageHoverDisposable
+  );
 }
 
-// this method is called when your extension is deactivated
+// this method is called when extension is deactivated
 export function deactivate() {}
